bb470228225c18b7efa6473d9b4ac89ee3374faa
ls1w <michelle_j.b@hotmail.com>
2023-11-30 11:58:44 -0300

    Tabuada V.1

---
+++ javascript/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let contador = 0;
+let numero;
+let resultado;
+let linha;
+
+function tabuadawhile(){
+
+    numero = parseInt(document.querySelector('#slcNumero').value);
+
+}
+
+while (contador <= 10){
+
+    resultado = slcNumero * contador
+    console.log (' X ' + contador + ' = ' +(slcNumero * contador));
+    console.log (resultado);
+    contador++;
+}
